4c2bae20b61c1d86215c96d3c4fffd6fa785d874
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2010-09-28 05:13:07 +0000

    Creating Umfrage directory

---
+++ language/spanish/blocks.php
@@ -0,0 +1,8 @@
+<?php
+// $Id: blocks.php 1 2007-10-30 11:28:51Z LupusC $
+//Traducción por debianus
+// Blocks
+define('_BL_POLL_EDIT_SHOWRESULTS', 'Mostrar resultados cuando la votación está expirada o el usuario ha votado ');
+define('_BL_POLL_EDIT_SHOWPERCENT', 'Mostrar porventaje en los resultados');
+define('_BL_POLL_EDIT_SHOWBAR', 'Mostrar la barra de resultados');
+?>
