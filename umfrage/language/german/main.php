f43759ea5e21b4f8d795800dcdfeb2229b254762
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2012-08-11 12:46:10 +0000

    Removing spaces from "define", which is required for Transifex per DCrusader

--- umfrage/language/german/main.php
+++ umfrage/language/german/main.php
@@ -6,21 +6,21 @@ define('_PL_TOTALVOTERS','Personen die insgesamt abgestimmt haben: %s');
 
 //%%%%%%	File Name index.php 	%%%%%
 define('_PL_POLLSLIST','Umfragenliste');
-define('_PL_LOGINTOVOTE','Um Abstimmen zu können müssen sie angemeldet sein.');
-define('_PL_ALREADYVOTED', 'Verzeihung, aber Sie haben schon abgestimmt.');
-define('_PL_ALREADYVOTED2', 'Sie haben bereits abgestimmt.');
-define('_PL_THANKSFORVOTE','Danke für Ihre Stimme!');
-define('_PL_SORRYEXPIRED', 'Verzeihung, aber die Umfrage wude schon beendet.');
-define('_PL_YOURPOLLAT', '%s, Ihre Stimme bei %s'); // 1st %s is user name, 2nd %s is site name
-define('_PL_PREV', 'Vorherige');
-define('_PL_NEXT', 'Nächste');
-define('_PL_POLLQUESTION', 'Umfragetitel');
-define('_PL_VOTERS', 'Stimmen');
-define('_PL_VOTES', 'Abgestimmt werden kann');
-define('_PL_EXPIRATION', 'Umfrage endet am');
-define('_PL_EXPIRED', 'ABGELAUFEN');
-define('_PL_EXPIREDON', 'Umfrage beendet am');
-define('_PL_ONLYREGISTERED','Abstimmung nur für angemeldete Benutzer');
+define('_PL_LOGINTOVOTE','Um Abstimmen zu kÃ¶nnen mÃ¼ssen sie angemeldet sein.');
+define('_PL_ALREADYVOTED','Verzeihung, aber Sie haben schon abgestimmt.');
+define('_PL_ALREADYVOTED2','Sie haben bereits abgestimmt.');
+define('_PL_THANKSFORVOTE','Danke fÃ¼r Ihre Stimme!');
+define('_PL_SORRYEXPIRED','Verzeihung, aber die Umfrage wude schon beendet.');
+define('_PL_YOURPOLLAT','%s, Ihre Stimme bei %s'); // 1st %s is user name, 2nd %s is site name
+define('_PL_PREV','Vorherige');
+define('_PL_NEXT','NÃ¤chste');
+define('_PL_POLLQUESTION','Umfragetitel');
+define('_PL_VOTERS','Stimmen');
+define('_PL_VOTES','Abgestimmt werden kann');
+define('_PL_EXPIRATION','Umfrage endet am');
+define('_PL_EXPIRED','ABGELAUFEN');
+define('_PL_EXPIREDON','Umfrage beendet am');
+define('_PL_ONLYREGISTERED','Abstimmung nur fÃ¼r angemeldete Benutzer');
 
 
 //%%%%%%	File Name umfragerenderer.php 	%%%%%
@@ -30,9 +30,9 @@ define('_PL_ENDEDAT','Beendet am %s');
 define('_PL_VOTE','Abstimmen');
 define('_PL_RESULTS','Endergebnis');
 define('_PL_STANDINGS','Zwischenstand');
-define('_PL_SHOW_ELECTIONSMODE','Das Ergebnis kann nicht öffentlich angezeigt werden.');
+define('_PL_SHOW_ELECTIONSMODE','Das Ergebnis kann nicht Ã¶ffentlich angezeigt werden.');
 define('_PL_MAILBODY1','Das Ergebnis der Abstimmung sieht wie folgt aus:');
-define('_PL_MAILBODY2',' Stimmen für: ');
+define('_PL_MAILBODY2',' Stimmen fÃ¼r: ');
 define('_PL_MAILBODY3',' Insgesamt wurden %d Stimmen abgegeben');
 define('_PL_HALFBLIND','Dies ist eine geheime Abstimmung bei der das Ergebnis erst nach Ablauf der Umfrage angezeigt wird.');
 define('_PL_FULLBLIND','Dies ist eine geheime Abstimmung bei der das Ergebnis weder w&auml;hrend der Umfrage, noch nach der Beendigung angezeigt wird. ');
